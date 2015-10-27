f1c0c7300a7739e392d8e9271dc1e4390ded92c6
helenzh <helenz2010@gmail.com>
2015-10-26 23:19:58 -0700

    Turn in

---
+++ Dropbox-demo/SettingViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingViewController.swift
+//  Dropbox-demo
+//
+//  Created by Helen Zhou on 10/26/15.
+//  Copyright © 2015 Helen Zhou. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        
+        scrollView.contentSize = imageView.frame.size
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
